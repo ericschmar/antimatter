11ded3174096acd4615a66f13cbf7c11da6f0688
Eric Schmar <>
2026-09-03 07:31:37 -0700

    delete react stuff

--- hutch.config.ts
+++
@@ -1,5 +0,0 @@
-// Retain Bun and its existing lockfile while Electrobun 2 delegates build
-// orchestration to Hutch.
-export default {
-	packageManager: "bun",
-};
--- src/mainview/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { MainViewApp } from "./app/MainViewApp";
-import "./index.css";
-
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Missing root element.");
-
-createRoot(rootElement).render(<MainViewApp />);
